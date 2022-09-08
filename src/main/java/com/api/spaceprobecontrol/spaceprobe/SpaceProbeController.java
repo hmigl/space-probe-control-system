f1d519adda7f6dab1e10fdf196f0887e0c1f7342
hmigl <88746406+hmigl@users.noreply.github.com>
2022-09-08 15:43:18 -0300

    feat: do nothing if planetId doesn't exist

--- src/main/java/com/api/spaceprobecontrol/spaceprobe/SpaceProbeController.java
+++ src/main/java/com/api/spaceprobecontrol/spaceprobe/SpaceProbeController.java
@@ -1,5 +1,6 @@
 package com.api.spaceprobecontrol.spaceprobe;
 
+import com.api.spaceprobecontrol.planet.PlanetRepository;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,9 +12,18 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/api")
 public class SpaceProbeController {
+
+    private final PlanetRepository planetRepository;
+
+    public SpaceProbeController(PlanetRepository planetRepository) {
+        this.planetRepository = planetRepository;
+    }
+
     @PostMapping("/planets/{id}/probes")
     void registerNewSpaceProbe(@PathVariable("id") Long id,
                                @RequestBody @Valid DesignationSpaceProbeRequest request) {
+        if (!planetRepository.existsById(id))
+            return;
         request.getSpaceProbes().forEach(System.out::println);
     }
 }
