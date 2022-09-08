c2be28ba249d1646800eb98ecdd511c0dafc9edf
hmigl <88746406+hmigl@users.noreply.github.com>
2022-09-08 10:31:10 -0300

    feat: first commit

---
+++ src/main/java/com/api/spaceprobecontrol/SpaceProbeControlApplication.java
@@ -0,0 +1,13 @@
+package com.api.spaceprobecontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceProbeControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpaceProbeControlApplication.class, args);
+    }
+
+}
