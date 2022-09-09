e51686fd7677160c7ce6cb5c96b0dfcc48a5ce5b
hmigl <88746406+hmigl@users.noreply.github.com>
2022-09-09 13:18:38 -0300

    test: add missing tests for showSpecificPlanet()

--- src/test/java/com/api/spaceprobecontrol/planet/PlanetControllerTest.java
+++ src/test/java/com/api/spaceprobecontrol/planet/PlanetControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.when;
@@ -36,5 +37,28 @@ class PlanetControllerTest {
                 .andExpect(jsonPath("$[0].yAxis").value(1))
                 .andExpect(jsonPath("$[1].xAxis").value(2))
                 .andExpect(jsonPath("$[1].xAxis").value(2));
+    }
+
+    @Test
+    @DisplayName("Should perform GET at '/api/planets/{id}' and return status 200")
+    void test2() throws Exception {
+        when(repository.findById(1L))
+                .thenReturn(Optional.of(new Planet(1, 1)));
+
+        mockMvc.perform(get("/api/planets/{id}", 1L))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()", is(3)))
+                .andExpect(jsonPath("$.xAxis").value(1))
+                .andExpect(jsonPath("$.yAxis").value(1));
+    }
+
+    @Test
+    @DisplayName("Should perform GET at '/api/planets/{id}' and return status 404")
+    void test3() throws Exception {
+        when(repository.findById(1L))
+                .thenReturn(Optional.empty());
+
+        mockMvc.perform(get("/api/planets/{id}", 1L))
+                .andExpect(status().isNotFound());
     }
 }
