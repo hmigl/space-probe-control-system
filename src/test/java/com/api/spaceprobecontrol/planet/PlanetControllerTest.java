6bee9a228317d4f3cf78831f9de8c6069cdf2f3f
hmigl <88746406+hmigl@users.noreply.github.com>
2022-09-09 14:33:43 -0300

    test: add tests for registerNewPlanet()
    Test it with both valid and invalid arguments.

--- src/test/java/com/api/spaceprobecontrol/planet/PlanetControllerTest.java
+++ src/test/java/com/api/spaceprobecontrol/planet/PlanetControllerTest.java
@@ -5,16 +5,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.List;
 import java.util.Optional;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @WebMvcTest(PlanetController.class)
 class PlanetControllerTest {
@@ -31,12 +33,12 @@ class PlanetControllerTest {
                 .thenReturn(List.of(new Planet(1, 1), new Planet(2, 2)));
 
         mockMvc.perform(get("/api/planets"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.size()", is(2)))
-                .andExpect(jsonPath("$[0].xAxis").value(1))
-                .andExpect(jsonPath("$[0].yAxis").value(1))
-                .andExpect(jsonPath("$[1].xAxis").value(2))
-                .andExpect(jsonPath("$[1].xAxis").value(2));
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.size()", is(2)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].xAxis").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].yAxis").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].xAxis").value(2))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].xAxis").value(2));
     }
 
     @Test
@@ -46,10 +48,10 @@ class PlanetControllerTest {
                 .thenReturn(Optional.of(new Planet(1, 1)));
 
         mockMvc.perform(get("/api/planets/{id}", 1L))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.size()", is(3)))
-                .andExpect(jsonPath("$.xAxis").value(1))
-                .andExpect(jsonPath("$.yAxis").value(1));
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.size()", is(3)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.xAxis").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.yAxis").value(1));
     }
 
     @Test
@@ -59,6 +61,26 @@ class PlanetControllerTest {
                 .thenReturn(Optional.empty());
 
         mockMvc.perform(get("/api/planets/{id}", 1L))
-                .andExpect(status().isNotFound());
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    @DisplayName("Should perform POST at '/api/planets' and return status 201")
+    void test4() throws Exception {
+        mockMvc.perform(post("/api/planets")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"xAxis\": 2, \"yAxis\": 2}"))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.header().exists("location"))
+                .andExpect(MockMvcResultMatchers.header().string("location", containsString("planets/")));
+    }
+
+    @Test
+    @DisplayName("Should perform POST at '/api/planets' and return status 400")
+    void test5() throws Exception {
+        mockMvc.perform(post("/api/planets")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{}"))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
     }
 }
