48f518bafcb0782a8dd3e1fbb29e21afa5dea1e8
hmigl <88746406+hmigl@users.noreply.github.com>
2022-09-20 17:14:55 -0300

    test: improve tests naming and fix failed one

--- src/test/java/com/api/spaceprobecontrol/planet/PlanetControllerTest.java
+++ src/test/java/com/api/spaceprobecontrol/planet/PlanetControllerTest.java
@@ -7,80 +7,75 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.List;
 import java.util.Optional;
 
-import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(PlanetController.class)
 class PlanetControllerTest {
     @Autowired
     private MockMvc mockMvc;
-
     @MockBean
     private PlanetRepository repository;
 
-    @Test
-    @DisplayName("Should perform GET at '/api/planets' and return status 200")
-    void test1() throws Exception {
-        when(repository.findAll())
-                .thenReturn(List.of(new Planet(1, 1), new Planet(2, 2)));
+    private static final String URI = "/api/planets";
 
-        mockMvc.perform(get("/api/planets"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.size()", is(2)))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[0].xAxis").value(1))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[0].yAxis").value(1))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[1].xAxis").value(2))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[1].xAxis").value(2));
+    @Test
+    @DisplayName("Should list all planets")
+    void shouldListAllPlanets() throws Exception {
+        given(repository.findAll())
+                .willReturn(List.of(new Planet(1, 1), new Planet(2, 2)));
+
+        mockMvc.perform(get(URI))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()", is(2)))
+                .andExpect(jsonPath("$[0].xAxis").value(1))
+                .andExpect(jsonPath("$[0].yAxis").value(1))
+                .andExpect(jsonPath("$[1].xAxis").value(2))
+                .andExpect(jsonPath("$[1].xAxis").value(2));
     }
 
     @Test
-    @DisplayName("Should perform GET at '/api/planets/{id}' and return status 200")
-    void test2() throws Exception {
-        when(repository.findById(1L))
-                .thenReturn(Optional.of(new Planet(1, 1)));
+    @DisplayName("Should show details about existent planet")
+    void shouldShowExistentPlanetDetails() throws Exception {
+        given(repository.findById(any()))
+                .willReturn(Optional.of(new Planet(1, 1)));
 
-        mockMvc.perform(get("/api/planets/{id}", 1L))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.size()", is(3)))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.xAxis").value(1))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.yAxis").value(1));
+        mockMvc.perform(get(URI + "/{id}", 1L))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()", is(4)))
+                .andExpect(jsonPath("$.xAxis").value(1))
+                .andExpect(jsonPath("$.yAxis").value(1));
     }
 
     @Test
-    @DisplayName("Should perform GET at '/api/planets/{id}' and return status 404")
-    void test3() throws Exception {
-        when(repository.findById(1L))
-                .thenReturn(Optional.empty());
+    @DisplayName("Should not find planet")
+    void shouldNotFindPlanet() throws Exception {
+        given(repository.findById(any()))
+                .willReturn(Optional.empty());
 
-        mockMvc.perform(get("/api/planets/{id}", 1L))
-                .andExpect(MockMvcResultMatchers.status().isNotFound());
+        mockMvc.perform(get(URI + "/{id}", 1L))
+                .andExpect(status().isNotFound());
     }
 
     @Test
-    @DisplayName("Should perform POST at '/api/planets' and return status 201")
-    void test4() throws Exception {
-        mockMvc.perform(post("/api/planets")
+    @DisplayName("Should register new planet")
+    void shouldRegisterNewPlanet() throws Exception {
+        mockMvc.perform(post(URI)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"xAxis\": 2, \"yAxis\": 2}"))
-                .andExpect(MockMvcResultMatchers.status().isCreated())
-                .andExpect(MockMvcResultMatchers.header().exists("location"))
-                .andExpect(MockMvcResultMatchers.header().string("location", containsString("planets/")));
-    }
+                .andExpect(status().isCreated());
 
-    @Test
-    @DisplayName("Should perform POST at '/api/planets' and return status 400")
-    void test5() throws Exception {
-        mockMvc.perform(post("/api/planets")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{}"))
-                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+        verify(repository, times(1)).save(any(Planet.class));
     }
 }
